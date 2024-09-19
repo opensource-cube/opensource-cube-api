bb1810caac20dc6975973a70cf1fef14a5b4ed12
joonhee.song <mike.urssu@gmail.com>
2024-09-19 19:32:50 +0900

    init

---
+++ src/main/kotlin/com/osscube/api/OpensourceCubeApiApplication.kt
@@ -0,0 +1,11 @@
+package com.osscube.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpensourceCubeApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<OpensourceCubeApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/osscube/api/OpensourceCubeApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.osscube.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OpensourceCubeApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
