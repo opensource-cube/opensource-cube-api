706a3d1c0eea55519f489566175eef4fe642b16f
joonhee.song <mike.urssu@gmail.com>
2024-09-21 00:57:39 +0900

    feat: 오픈소스가 존재하는지 확인

--- src/main/kotlin/com/osscube/api/domain/model/repository/OpenSourceRepository.kt
+++ src/main/kotlin/com/osscube/api/domain/model/repository/OpenSourceRepository.kt
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface OpenSourceRepository : JpaRepository<OpenSource, Long>
+interface OpenSourceRepository : JpaRepository<OpenSource, Long> {
+    fun existsByNameAndOriginUrl(name: String, originUrl: String): Boolean
+}
---
+++ src/test/kotlin/com/osscube/api/domain/model/repository/OpenSourceRepositoryTest.kt
@@ -0,0 +1,57 @@
+package com.osscube.api.domain.model.repository
+
+import com.osscube.api.config.TestContainers
+import com.osscube.api.domain.model.entity.OpenSource
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.testcontainers.junit.jupiter.Testcontainers
+
+@SpringBootTest
+@ActiveProfiles
+@Testcontainers
+class OpenSourceRepositoryTest : TestContainers() {
+    @Autowired
+    private lateinit var openSourceRepository: OpenSourceRepository
+
+    @BeforeEach
+    fun cleansing() {
+        openSourceRepository.deleteAllInBatch()
+    }
+
+    @DisplayName("오픈소스가 없으면 false를 반환한다.")
+    @Test
+    fun existsByNameAndOriginUrlReturnFalseIfNotExist() {
+        // given
+        val openSource = OpenSource("name", "origin url")
+        openSourceRepository.save(openSource)
+
+        // when
+        val areExist = listOf(
+            openSourceRepository.existsByNameAndOriginUrl(openSource.name, "other origin url"),
+            openSourceRepository.existsByNameAndOriginUrl("other name", openSource.originUrl),
+            openSourceRepository.existsByNameAndOriginUrl("other name", "otherOrigin url")
+        )
+
+        // then
+        areExist.forEach { isExist -> assertThat(isExist).isFalse() }
+    }
+
+    @DisplayName("오픈소스가 이미 저장되어 있으면 true를 반환한다.")
+    @Test
+    fun existsByNameAndOriginUrlReturnTrueIfExist() {
+        // given
+        val openSource = OpenSource("name", "origin url")
+        openSourceRepository.save(openSource)
+
+        // when
+        val isExist = openSourceRepository.existsByNameAndOriginUrl(openSource.name, openSource.originUrl)
+
+        // then
+        assertThat(isExist).isTrue()
+    }
+}
