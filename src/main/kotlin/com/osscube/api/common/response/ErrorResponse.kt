9d889e0088299c75367097049e340d94ae371018
joonhee.song <mike.urssu@gmail.com>
2024-09-21 01:04:22 +0900

    feat: 새로운 오픈소스 저장 시 이미 저장된 오픈소스인지 확인

---
+++ src/main/kotlin/com/osscube/api/common/response/ErrorResponse.kt
@@ -0,0 +1,19 @@
+package com.osscube.api.common.response
+
+import java.time.Clock
+import java.time.LocalDateTime
+import org.springframework.http.HttpStatus
+
+class ErrorResponse(
+    val timestamp: LocalDateTime,
+    val status: Int,
+    val error: String,
+    val message: String
+) {
+    constructor(httpStatus: HttpStatus, errorCode: String, message: String) : this(
+        timestamp = LocalDateTime.now(Clock.systemDefaultZone()),
+        status = httpStatus.value(),
+        error = errorCode,
+        message = message
+    )
+}
--- src/test/kotlin/com/osscube/api/domain/service/OpenSourceServiceTest.kt
+++ src/test/kotlin/com/osscube/api/domain/service/OpenSourceServiceTest.kt
@@ -2,7 +2,12 @@ package com.osscube.api.domain.service
 
 import com.osscube.api.config.TestContainers
 import com.osscube.api.domain.dto.OpenSourceSaveRequestDto
+import com.osscube.api.domain.exception.open_source.OpenSourceAlreadyExistsException
+import com.osscube.api.domain.model.entity.OpenSource
+import com.osscube.api.domain.model.repository.OpenSourceRepository
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -16,6 +21,14 @@ import org.testcontainers.junit.jupiter.Testcontainers
 class OpenSourceServiceTest : TestContainers() {
     @Autowired
     private lateinit var openSourceService: OpenSourceService
+
+    @Autowired
+    private lateinit var openSourceRepository: OpenSourceRepository
+
+    @BeforeEach
+    fun cleansing() {
+        openSourceRepository.deleteAllInBatch()
+    }
 
     @DisplayName("오픈소스명, 오픈소스 출처를 받아서 오픈소스를 저장한다.")
     @Test
@@ -34,5 +47,19 @@ class OpenSourceServiceTest : TestContainers() {
         assertThat(responseDto)
             .extracting("name", "originUrl")
             .contains(name, originUrl)
+    }
+
+    @DisplayName("이미 저장된 오픈소스를 다시 저장하면 예외가 발생한다.")
+    @Test
+    fun saveOpenSourceThrowsExceptionIfOpenSourceExists() {
+        // given
+        val requestDto = OpenSourceSaveRequestDto("name", "originUrl")
+
+        val openSource = OpenSource.of(requestDto)
+        openSourceRepository.save(openSource)
+
+        // when // then
+        assertThatThrownBy { openSourceService.saveOpenSource(requestDto) }
+            .isInstanceOf(OpenSourceAlreadyExistsException::class.java)
     }
 }
