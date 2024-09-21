513a2c85b2cd00ad9948b7b4083c46b57c1aa2a0
joonhee.song <mike.urssu@gmail.com>
2024-09-22 00:32:08 +0900

    feat: 오픈소스 버전이 이미 추가됐는지 확인 [#8]

--- src/main/kotlin/com/osscube/api/domain/model/repository/OpenSourceVersionRepository.kt
+++ src/main/kotlin/com/osscube/api/domain/model/repository/OpenSourceVersionRepository.kt
@@ -1,8 +1,11 @@
 package com.osscube.api.domain.model.repository
 
+import com.osscube.api.domain.model.entity.OpenSource
 import com.osscube.api.domain.model.entity.OpenSourceVersion
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface OpenSourceVersionRepository : JpaRepository<OpenSourceVersion, Long>
+interface OpenSourceVersionRepository : JpaRepository<OpenSourceVersion, Long> {
+    fun existsByOpenSourceAndVersion(openSource: OpenSource, version: String): Boolean
+}
