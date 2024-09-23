1ad19de42fd0744a9f364eb4513681302ec5841d
joonhee.song <mike.urssu@gmail.com>
2024-09-23 21:26:54 +0900

    feat: 오픈소스에 새로운 버전을 추가하는 api 구현 [#8]

--- src/main/kotlin/com/osscube/api/domain/dto/OpenSourceVersionAddRequestDto.kt
+++ src/main/kotlin/com/osscube/api/domain/dto/OpenSourceVersionAddRequestDto.kt
@@ -1,6 +1,13 @@
 package com.osscube.api.domain.dto
 
+import com.osscube.api.application.request.OpenSourceVersionAddRequest
+
 class OpenSourceVersionAddRequestDto(
     val version: String,
     val sourceUrl: String?
-)
+) {
+    companion object {
+        fun of(request: OpenSourceVersionAddRequest) =
+            OpenSourceVersionAddRequestDto(request.version, request.sourceUrl)
+    }
+}
